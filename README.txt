ed217563c4a57cb031361b43660d67ebebb6a7e6
lankaxadd <nipuniliyanage65@gmail.com>
2026-05-22 12:38:56 +0530

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,14 @@
+R Enjoy And X Life
+
+Admin Login:
+Email: admin@gmail.com
+Password: 1234
+
+Firebase Project:
+pos-web-391e3
+
+Hosting Commands:
+
+firebase login
+firebase init hosting
+firebase deploy
---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAItZmB5yFduZvoYt8T3oxFDnqmUit_B4Q",
+  authDomain: "pos-web-391e3.firebaseapp.com",
+  projectId: "pos-web-391e3",
+  storageBucket: "pos-web-391e3.firebasestorage.app",
+  messagingSenderId: "745467335661",
+  appId: "1:745467335661:web:e541ea50f45f4326954de4"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
